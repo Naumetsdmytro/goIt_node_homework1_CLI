c71c184b706ae017cd54cd52a213133a1a2d1145
Naumetsdmytro <113528509+Naumetsdmytro@users.noreply.github.com>
2023-05-22 16:00:21 +0300

    create CLI

---
+++ contacts.js
@@ -0,0 +1,47 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+const getAllContacts = async () => {
+  const contacts = await fs.readFile(contactsPath);
+  return JSON.parse(contacts);
+};
+
+const getContactById = async (id) => {
+  const contacts = await getAllContacts();
+  const contact = contacts.find((contact) => contact.id === id);
+  return contact || null;
+};
+
+const removeContact = async (id) => {
+  const contacts = await getAllContacts();
+  const index = contacts.findIndex((contact) => contact.id === id);
+
+  if (index === -1) {
+    return null;
+  }
+  const [deletedContact] = contacts.splice(index, 1);
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return deletedContact;
+};
+
+const addContact = async (data) => {
+  const contacts = await getAllContacts();
+
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+};
+
+module.exports = {
+  addContact,
+  removeContact,
+  getAllContacts,
+  getContactById,
+};
